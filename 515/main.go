89ffc10a4630b3ed4ef2b447122270601253ab59
Momen Kadry <kadry.momen6@proton.me>
2024-12-26 00:07:22 +0200

    Added 515

---
+++ 515/main.go
@@ -0,0 +1,39 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func largestValues(root *TreeNode) []int {
+	q := []*TreeNode{root}
+
+	if root == nil {
+		return []int{}
+	}
+
+	maxVals := []int{}
+
+	for len(q) > 0 {
+		levelSize := len(q)
+		maxInLevel := -1 << 31
+
+		for i := 0; i < levelSize; i++ {
+			currentNode := q[0]
+			q = q[1:]
+
+			if currentNode.Val > int(maxInLevel) {
+				maxInLevel = currentNode.Val
+			}
+			if currentNode.Left != nil {
+				q = append(q, currentNode.Left)
+			}
+			if currentNode.Right != nil {
+				q = append(q, currentNode.Right)
+			}
+		}
+		maxVals = append(maxVals, maxInLevel)
+	}
+	return maxVals
+}
