3324ad0969153197473df3dff5c3dfcb80d475c8
Momen Kadry <kadry.momen6@proton.me>
2024-12-29 01:25:35 +0200

    Added 1071

---
+++ 1071/main.go
@@ -0,0 +1,18 @@
+package main
+
+func gcdOfStrings(str1 string, str2 string) string {
+  if str1 + str2 != str2 + str1 {
+    return ""
+  } else {
+    return str1[:GCD(len(str1), len(str2))]
+  }
+}
+
+func GCD(a, b int) int {
+    for b != 0 {
+        t := b
+        b = a % b
+        a = t
+    }
+    return a
+}
