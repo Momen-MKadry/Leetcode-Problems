bcbf42bf00f5cdbd17717a3258d49dc9b036a77d
Momen Kadry <kadry.momen6@proton.me>
2024-12-29 01:26:00 +0200

    Added 151

---
+++ 151/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "strings"
+
+func reverseWords(s string) string {
+	var words []string
+	var currentWord strings.Builder
+
+	for _, char := range s {
+		if char == ' ' && currentWord.Len() > 0 {
+			words = append(words, currentWord.String())
+			currentWord.Reset()
+		} else if char != ' ' {
+			currentWord.WriteRune(char)
+		}
+	}
+
+	if currentWord.Len() > 0 {
+		words = append(words, currentWord.String())
+	}
+
+	for i, j := 0, len(words)-1; i < j; i, j = i+1, j-1 {
+		words[i], words[j] = words[j], words[i]
+	}
+
+	return strings.Join(words, " ")
+
+	// return string(runes)
+}
+
+func main() {
+	str := "   my    sky      "
+	str = reverseWords(str)
+	print(str)
+	print("G")
+}
