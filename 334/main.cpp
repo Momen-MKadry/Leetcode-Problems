9329082aefbb5b4d74dcba89c2e2d41d85d397c3
Momen Kadry <kadry.momen6@proton.me>
2024-12-30 00:20:50 +0200

    Added 334

---
+++ 334/main.cpp
@@ -0,0 +1,55 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <stack>
+
+using namespace std;
+
+#define ll long long
+
+class Solution {
+public:
+  bool increasingTriplet(vector<int> &nums) {
+    vector<int> minBeforeI(nums.size());
+    vector<int> maxAfterI(nums.size());
+    ll min = INT_MAX;
+    ll max = INT_MIN;
+
+    for (ll i = 0; i < nums.size(); i++) {
+      if (nums[i] < min) {
+        min = nums[i];
+        minBeforeI[i] = nums[i];
+      }
+      minBeforeI[i] = min;
+    }
+    for (ll i = nums.size() - 1; i >= 0; i--) {
+      if (nums[i] > max) {
+        max = nums[i];
+        maxAfterI[i] = nums[i];
+      }
+      maxAfterI[i] = max;
+    }
+
+    for (ll i = 0; i < nums.size(); i++) {
+      if (minBeforeI[i] != nums[i] && maxAfterI[i] != nums[i]) {
+        return true;
+      }
+    }
+
+    // for (int i = 0; i < maxAfterI.size(); i++) {
+    //   cout << maxAfterI[i] << " ";
+    // }
+    // cout << endl;
+    // for (int i = 0; i < minBeforeI.size(); i++) {
+    //   cout << minBeforeI[i] << " ";
+    // }
+    // cout << endl;
+
+    return false;
+  }
+};
+
+int main() {
+  Solution s;
+  vector<int> nums = {6, 7, 1, 2};
+  cout << s.increasingTriplet(nums);
+}
