a3ae98b351910a0fcb8664740a3b09867ff65223
Momen Kadry <kadry.momen6@proton.me>
2024-12-30 22:36:36 +0200

    Added another solution for 443

---
+++ 443/anotherSol.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+  int compress(vector<char> &chars) {
+    int v = 0, r = 0;
+
+    while (v < chars.size()) {
+      int gl = 1;
+      while (v + gl < chars.size() && chars[v + gl] == chars[v]) {
+        gl++;
+      }
+      chars[r++] = chars[v];
+      if (gl > 1) {
+        for (auto c : to_string(gl))
+          chars[r++] = c;
+      }
+      v += gl;
+    }
+    return chars.size();
+  }
+};
