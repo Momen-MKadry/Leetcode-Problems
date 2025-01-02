a3c2be961fb702fbeb1790b622fbcb3cf3eb354e
Momen Kadry <kadry.momen6@proton.me>
2025-01-02 23:57:37 +0200

    1493

---
+++ 1493/main.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+  int longestSubarray(vector<int> &nums) {
+    int l = 0, res = 0, n = nums.size(), zeros = 0;
+
+    for (int i = 0; i < n; i++) {
+      zeros += (nums[i] == 0);
+
+      while (zeros > 1) {
+        zeros -= (nums[l] == 0);
+        l++;
+      }
+      res = max(res, i - l);
+    }
+
+    return res;
+  }
+};
