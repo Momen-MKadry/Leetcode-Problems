2008a78c68dff6d092ad8967ad0bbe5082fe2cfb
Momen Kadry <kadry.momen6@proton.me>
2025-01-02 23:57:29 +0200

    1004

---
+++ 1004/main.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+  int longestOnes(vector<int> &nums, int k) {
+    int l = 0, r = 0, firstZeroIdx = -1, finalRes = 0, n = nums.size();
+    if (n == 1) {
+      if (k > 0)
+        return 1;
+      else {
+        if (nums[0])
+          return 1;
+        else
+          return 0;
+      }
+    }
+    int currK = k;
+    while (r < n) {
+
+      if (nums[r]) {
+        r++;
+
+      } else if (!nums[r]) {
+        if (firstZeroIdx == -1)
+          firstZeroIdx = r;
+        if (currK > 0) {
+          currK--;
+          r++;
+
+        } else {
+
+          l = firstZeroIdx + 1;
+          r = firstZeroIdx + 1;
+          currK = k;
+          firstZeroIdx = -1;
+        }
+      }
+      finalRes = max(finalRes, r - l);
+    }
+    return finalRes;
+  }
+};
