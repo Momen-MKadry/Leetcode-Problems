cb7b334ac6fafd4b4880e289103afb3d13c92b27
Momen Kadry <kadry.momen6@proton.me>
2025-01-03 21:59:50 +0200

    724

---
+++ 724/main.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+  int pivotIndex(vector<int> &nums) {
+    int arrSum = 0, currSum = 0, n = nums.size();
+    for (int i = 0; i < n; i++) {
+      arrSum += nums[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+      currSum += nums[i];
+
+      if ((currSum - nums[i]) == (arrSum - currSum))
+        return i;
+    }
+    return -1;
+  }
+};
