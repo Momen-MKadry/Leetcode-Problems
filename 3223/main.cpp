a0103f4feaeee07a4d2747ff41e64658e9611b27
Mo'men Kadry <kadry.momen6@proton.me>
2025-01-13 12:17:37 +0200

    3223

---
+++ 3223/main.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+  int minimumLength(string s) {
+    if (s.length() <= 2)
+      return s.length();
+
+    int freq[26];
+    int cnt = 0;
+
+    for (auto ch : s) {
+      ++freq[ch - 'a'];
+    }
+
+    for (auto i : freq) {
+      if (i >= 3) {
+        if (i % 2)
+          cnt += (i - 1);
+        else
+          cnt += (i - 2);
+      }
+    }
+    return s.length() - cnt;
+  }
+};
