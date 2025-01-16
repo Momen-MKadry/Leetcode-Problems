749d908a46bd2115c77810ed4704aa4a1a9866e8
Mo'men Kadry <kadry.momen6@proton.me>
2025-01-16 14:19:50 +0200

    2533-BITWISE-XOR-OF-ALL-PAIRINGS Stats: Time: 0 ms (100%), Space: 64.2 MB (82.93%) - LeetHub

---
+++ 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+
+        if (nums1.size() % 2 == 0 && nums2.size() % 2 == 0) {
+            return 0;
+        }
+        if (nums1.size() % 2 == 0) {
+            int xord = 0;
+            for (int i = 0; i < nums1.size(); ++i)
+                xord ^= nums1[i];
+            return xord;
+        }
+        if (nums2.size() % 2 == 0) {
+            int xord = 0;
+            for (int i = 0; i < nums2.size(); ++i)
+                xord ^= nums2[i];
+            return xord;
+        }
+        
+
+        int xord = 0;
+
+        for (int i = 0; i < nums1.size(); ++i) {
+            xord ^= nums1[i];
+        }
+
+        for (int i = 0; i < nums2.size(); ++i) {
+            xord ^= nums2[i];
+        }
+
+        return xord;
+    }
+};
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 ## My solutions for some leetcode Problems
+
+<!---LeetCode Topics Start-->
+# LeetCode Topics
+## Array
+|  |
+| ------- |
+| [2533-bitwise-xor-of-all-pairings](https://github.com/Momen-MKadry/Leetcode-Problems/tree/master/2533-bitwise-xor-of-all-pairings) |
+## Bit Manipulation
+|  |
+| ------- |
+| [2533-bitwise-xor-of-all-pairings](https://github.com/Momen-MKadry/Leetcode-Problems/tree/master/2533-bitwise-xor-of-all-pairings) |
+## Brainteaser
+|  |
+| ------- |
+| [2533-bitwise-xor-of-all-pairings](https://github.com/Momen-MKadry/Leetcode-Problems/tree/master/2533-bitwise-xor-of-all-pairings) |
+<!---LeetCode Topics End-->
