d41c6ebe016e6bba11db4f67595d70c728fa9e0d
Mo'men Kadry <kadry.momen6@proton.me>
2025-01-16 17:20:37 +0200

    1894-MERGE-STRINGS-ALTERNATELY Stats: Time: 0 ms (100%), Space: 8.75 MB (11.96%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+  string mergeAlternately(string word1, string word2) {
+    int w1l = word1.length();
+    int w2l = word2.length();
+    string maxWord = word2, minWord = word1;
+    int minL = w1l;
+    string result = "";
+    int total = w1l + w2l;
+
+    if (w1l > w2l) {
+      minL = w2l;
+      minWord = word2;
+      maxWord = word1;
+    }
+
+    int k = 0;
+
+    for (int i = 0; i < minL*2; i += 2) {
+      result += word1[k];
+      result += word2[k];
+      k++;
+      //cout << result << endl;
+    }
+
+    maxWord.erase(0, k);
+    //cout << maxWord << endl;
+    if(maxWord.length())
+      result += maxWord;
+    return result;
+  }
+};
