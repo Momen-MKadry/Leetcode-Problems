6078c6e9e33227e584c5d9e1373fedf13c070afe
Mo'men Kadry <kadry.momen6@proton.me>
2025-01-17 22:21:29 +0200

    1319-UNIQUE-NUMBER-OF-OCCURRENCES Stats: Time: 0 ms (100%), Space: 12.1 MB (9.57%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        set<int> nums;
         set<int> uniqueFreq;
         unordered_map<int, int> freq;
 
-        for(auto i : arr) {
-            nums.insert(i);
+        for(auto i : arr) 
             ++freq[i];
-        }
+
         for(auto i : freq) {
+            if(uniqueFreq.contains(i.second))
+                return false;
             uniqueFreq.insert(i.second);
         }
 
-        return (nums.size() == uniqueFreq.size());
+        return true;
     }
 };
