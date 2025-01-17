044ce91c3fa2a6f38a3ee2fe2fccc72bbcc1ae91
Mo'men Kadry <kadry.momen6@proton.me>
2025-01-17 11:19:20 +0200

    2792-NEIGHBORING-BITWISE-XOR Stats: Time: 0 ms (100%), Space: 263.9 MB (48.13%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
@@ -1,28 +1,13 @@
 class Solution {
 public:
     bool doesValidArrayExist(vector<int>& derived) {
-        vector<int> original;
-        int size = derived.size();
-        original.push_back(0);
-
-        for (int i = 0; i < size; ++i) {
-            if (i == size - 1) {
-                if (original[i] ^ original[0] == derived[i])
-                    return true;
-            }
-            original.push_back(derived[i] ^ original[i]);
+        int xord = 0;
+        for (auto i : derived) {
+            xord ^= i;
         }
-        original.clear();
-        original.push_back(1);
-
-        for (int i = 0; i < size; ++i) {
-            if (i == size - 1) {
-                if (original[i] ^ original[0] == derived[i])
-                    return true;
-            }
-            original.push_back(derived[i] ^ original[i]);
-        }
-
-        return false;
+        if (xord == 0)
+            return true;
+        else
+            return false;
     }
 };
