0be1460f0b888fbf06c6ce3c02941963a74bc21e
Mo'men Kadry <kadry.momen6@proton.me>
2025-03-06 22:25:10 +0200

    3227-FIND-MISSING-AND-REPEATED-VALUES Stats: Time: 0 ms (100%), Space: 25.5 MB (97.9%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        vector<int> ans = {0, 0};
+        vector<int> ans;
         int size = grid.size();
-        int matSize = (size * size);
-        vector<int> freq(matSize+1, 0);
-        vector<int> list;
-        for(int i = 0; i < size; ++i) {
-            for(int j = 0; j < size; ++j) {
-                list.push_back(grid[i][j]);
+        int matSize = size * size;
+
+        unsigned int actualSum = 0, actualSumSquared = 0;
+        unsigned int trueSum = 0, trueSumSquared = 0;
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                actualSum += grid[i][j];
+                actualSumSquared += (grid[i][j] * grid[i][j]);
             }
         }
 
-        for(int i = 0; i < matSize; ++i)
-            ++freq[list[i]];
-
-        for(int i = 0; i < matSize+1; ++i) {
-            if(freq[i] == 0)
-                ans[1] = i;
-            if(freq[i] == 2)
-                ans[0] = i;
+        for (int i = 0; i < matSize + 1; ++i) {
+            trueSum += i;
+            trueSumSquared += (i * i);
         }
+
+        int func1 = actualSum - trueSum;
+        int func2 = actualSumSquared - trueSumSquared;
+
+        int func3 = func2 / func1;
+        int r = (func3 + func1) / 2;
+        int m = -1 * (func1 - r);
+
+        ans.push_back(r);
+        ans.push_back(m);
 
         return ans;
     }
